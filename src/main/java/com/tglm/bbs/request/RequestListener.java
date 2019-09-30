d8edf30455e41054a34248c9238e003b79bf54b0
tglm <mnillx@outlook.com>
2019-09-30 20:22:47 +0800

    存

--- src/main/java/com/tglm/bbs/request/RequestListener.java
+++ src/main/java/com/tglm/bbs/request/RequestListener.java
@@ -1,28 +1,18 @@
 package com.tglm.bbs.request;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.annotation.WebListener;
 
 /**
  * @author mlgt
  * @date 2019/9/29
  */
-@Component
+@WebListener
 public class RequestListener implements ServletRequestListener {
 
     private ThreadLocal<ThreadContext> threadContextThreadLocal = new ThreadLocal<>();
 
-    final private RedisTemplate redisTemplate;
-
-    @Autowired
-    public RequestListener(RedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     @Override
     public void requestDestroyed(ServletRequestEvent sre) {
@@ -31,12 +21,13 @@ public class RequestListener implements ServletRequestListener {
 
     @Override
     public void requestInitialized(ServletRequestEvent sre) {
-        HttpServletRequest request = (HttpServletRequest) sre.getServletRequest();
-        String sessionId = (String) request.getAttribute("sessionId");
-        Session session = (Session) redisTemplate.opsForValue().get(sessionId);
-        ThreadContext threadContext = new ThreadContext();
-        threadContext.setSession(session);
-        threadContextThreadLocal.set(threadContext);
+//        HttpServletRequest request = (HttpServletRequest) sre.getServletRequest();
+//        String sessionId = (String) request.getAttribute("sessionId");
+////        Session session = (Session) redisTemplate.opsForValue().get(sessionId);
+////        ThreadContext threadContext = new ThreadContext();
+////        threadContext.setSession(session);
+//        threadContextThreadLocal.set(threadContext);
+
 
     }
 
